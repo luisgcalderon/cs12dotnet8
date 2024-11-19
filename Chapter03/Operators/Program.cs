a1cd1f8747ca056c8825a8604a73816bc8715c9f
Luis Gerardo Calderon Contreras <luisg.calderon@devcalcon.com>
2024-11-19 02:02:27 +0000

    logical

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -4,55 +4,34 @@ int b = a++;
 WriteLine($"a is {a}, b is {b}");
 #endregion
 
-#region Using prfix
 int c = 3;
 int d = ++c; // Prefix means increment c before assigning it.
 WriteLine($"c is {c}, d is {d}");
-#endregion
 
-#region Exploring logical operators
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}. f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+double g = 11.0;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g / f}");
+
 bool p = true;
 bool q = false;
 
-WriteLine($"AND  | p    | q    ");
+WriteLine($"AND  | p   |  q   ");
 WriteLine($"p    | {p & p,-5} | {p & q,-5} ");
 WriteLine($"q    | {q & p,-5} | {q & q,-5} ");
 WriteLine();
-WriteLine($"OR   | p    | q    ");
+WriteLine($"OR  | p   |  q   ");
 WriteLine($"p    | {p | p,-5} | {p | q,-5} ");
 WriteLine($"q    | {q | p,-5} | {q | q,-5} ");
 WriteLine();
-WriteLine($"XOR   | p    | q    ");
+WriteLine($"XOR  | p  |  q  ");
 WriteLine($"p    | {p ^ p,-5} | {p ^ q,-5} ");
 WriteLine($"q    | {q ^ p,-5} | {q ^ q,-5} ");
-#endregion
-
-#region ConditionalOperator
-WriteLine();
-// Note that DoStuff() returns true;
-WriteLine($"p & DoStuff() = {p & DoStuff()}");
-WriteLine($"q & DoStuff() = {q & DoStuff()}");
-
-WriteLine();
-WriteLine($"p && DoStuff() = {p && DoStuff()}");
-WriteLine($"q && DoStuff() = {q && DoStuff()}");
-#endregion
-
-WriteLine();
-int x = 10;
-int y = 6;
-
-WriteLine("Expression   |   Decimal |   Binary");
-WriteLine("-----------------------------------");
-WriteLine($"x           |   {x,7} | {x:B8}");
-WriteLine($"y           |   {y,7} | {y:B8}");
-WriteLine($"x & y       |   {x & y,7} | {(x & y):B8}");
-WriteLine($"x | y       |   {x | y,7} | {(x | y):B8}");
-WriteLine($"x ^ y       |   {x ^ y,7} | {(x ^ y):B8}");
-#region 
-static bool DoStuff()
-{
-    WriteLine("I am doing some stuff.");
-    return true;
-}
-#endregion
